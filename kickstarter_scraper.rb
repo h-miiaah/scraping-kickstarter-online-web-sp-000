dcf29d979fdda0dbfa6c832c8bd43f88dfa267c0
Haim D <haim11293@aol.com>
2019-11-09 13:15:36 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,6 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  projects = {}
 end
 
-create_project_hash
